21af70d60699aafe59e82daa5d276600350c3138
Daniel Dakhno <d.dakhno@streamergy.de>
2025-01-13 16:04:15 +0100

    added blob detection using cv2

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import time
 import struct
 import statistics
 import subprocess
-import tkinter
 import sys
 import cv2
 import numpy
@@ -26,29 +25,32 @@ PIXEL_SIZE = 120  # Size of each pixel in pixels
 GRID_SIZE = 8    # 8x8 grid
 
 # Create the main window
-root = tkinter.Tk()
-root.title("Thermal Camera")
 
 last_person_count = 1
 
-# Create a Canvas to draw on
-canvas = tkinter.Canvas(root, width=PIXEL_SIZE * GRID_SIZE, height=PIXEL_SIZE * GRID_SIZE)
-canvas.pack()
+
+img = numpy.zeros((8, 8), dtype=numpy.uint8)
+
+params = cv2.SimpleBlobDetector_Params()
+params.filterByCircularity = False
+params.filterByConvexity = False
+params.filterByInertia = False
+params.filterByColor = False      # Filter by color
+params.filterByArea = True       # Filter blobs by area
+params.minArea = 5               # Minimum area
+blob_detector = cv2.SimpleBlobDetector_create(params)
 
 def set_pixel(x, y, relative):
-    x1 = x * PIXEL_SIZE
-    y1 = y * PIXEL_SIZE
-    x2 = x1 + PIXEL_SIZE
-    y2 = y1 + PIXEL_SIZE
-    # Draw a rectangle (pixel)
-    canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=f'#{int(255 * relative):02x}00{int(255 * (1 - relative)):02x}')
+    pass
 
 while True:
     pixel_count = 64
-    data  = bus.read_i2c_block_data(0x69, 0x80     , 32)
-    data += bus.read_i2c_block_data(0x69, 0x80 + 32, 32)
-    data += bus.read_i2c_block_data(0x69, 0x80 + 64, 32)
-    data += bus.read_i2c_block_data(0x69, 0x80 + 96, 32)
+    data = []
+    read_count = 0
+    chunk_size = 16
+    while read_count < 128:
+        data += bus.read_i2c_block_data(0x69, 0x80 + read_count, chunk_size)
+        read_count += chunk_size
     temps = struct.unpack('<' + ('h' * pixel_count), bytes(data))
     temps = [i / 4 for i in temps]
     
@@ -62,36 +64,32 @@ while True:
         time.sleep(0.1)
         continue
 
-    warm_pixels = 0
-
     def get_temp(x, y):
         return normalized[63 - (y + x * 8)]
     
     for x in range(8):
         for y in range(8):
             temp = get_temp(x, y)
-            if temp < 5:
-                relative = 0
+            if temp < THRESHOLD:
+                img[x, y] = 0
             else:
-                relative = temp / max_temp
-                warm_pixels += 1
-            try:
-                set_pixel(x, y, relative)
-            except:
-                print('error setting pixel')
-                pass
+                img[x, y] = 255
+                # relative = temp / max_temp
+                # img[x, y] = int(relative * 127 + 128)
 
-    was_warm = get_temp(0, ROW) > THRESHOLD
+    blobs = blob_detector.detect(img)
 
-    person_count = 1 if was_warm else 0
+    # print(img)
 
-    for i in range(7):
-        temp = get_temp(i, ROW)
-        is_warm = temp > THRESHOLD
-        if is_warm is not was_warm:
-            if is_warm:
-                person_count += 1
-            was_warm = is_warm
+    print(blobs)
+
+    im_with_keypoints = cv2.drawKeypoints(img, blobs, numpy.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+    # Show keypoints
+    cv2.imshow("Keypoints", im_with_keypoints)
+    cv2.waitKey(1)
+
+    person_count = len(blobs)
 
     if person_count is not last_person_count:
         last_person_count = person_count
@@ -99,16 +97,16 @@ while True:
         print(person_count)
 
         if person_count == 0:
-            # subprocess.run(['xdg-screensaver', 'activate'])
-            subprocess.run(['notify-send', 'locked'])
-            tkinter.mainloop()
+            subprocess.run(['xdg-screensaver', 'activate'])
+            # subprocess.run(['notify-send', 'locked'])
         elif person_count == 1:
             subprocess.run(['ddcutil', 'setvcp', '10', '0'])
         elif person_count == 2:
             subprocess.run('/home/daniel/bin/minimize-chrome')
             subprocess.run(['ddcutil', 'setvcp', '10', '40'])
 
-    root.update_idletasks()
-    root.update()
-    
-    time.sleep(1)
+    try:
+        time.sleep(1)
+    except:
+        cv2.destroyAllWindows()
+        break
